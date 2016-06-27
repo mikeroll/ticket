07fb3e7b84dd78dfb54b28de103bb0c7f26a7122
Mikhail Bulash <scaryspiderpig@gmail.com>
2016-06-27 11:45:19 +0300

    Fix entrypoint

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'ticket=ticket:cli'
+            'ticket=ticket.ticket:cli'
         ]
     },
     install_requires=[
