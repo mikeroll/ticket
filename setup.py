91e59e5635d3c5dd50b2acd2bb0e26aeda6937d1
Mikhail Bulash <scaryspiderpig@gmail.com>
2016-06-27 11:42:24 +0300

    Requirements cleanup

--- setup.py
+++ setup.py
@@ -16,14 +16,8 @@ setup(
     install_requires=[
         "appdirs==1.4.0",
         "click==6.6",
-        "jira==1.0.3",
-        "oauthlib==1.1.1",
         "PyYAML==3.11",
-        "requests-oauthlib==0.6.1",
-        "requests-toolbelt==0.6.2",
-        "requests==2.10.0",
-        "six==1.10.0",
         "tabulate==0.7.5",
-        "tlslite==0.4.9",
+        "jira"
     ]
 )
